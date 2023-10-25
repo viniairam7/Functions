bcd7228fbbd16ec9d7723ec8827d050800224261
Airam <139919189+viniairam7@users.noreply.github.com>
2023-10-25 01:36:45 -0300

    Create Special Print.py

---
+++ Special Print.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 5
+    print('~' * tam)
+    print(f'   {msg}')
+    print('~' * tam)
+
+
+#Programa Principal
+escreva('"E o Ballon De Oro vai para: ')
+escreva('"CRISTIANO RONALDO!')
+escreva('"SIUUUUUU"')
